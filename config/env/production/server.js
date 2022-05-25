dfaa94436e6766c8495a15f341f30741a2d427e9
Melanie Rodas <mrodasl@uni.pe>
2022-05-25 14:19:10 -0500

    Update production/server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,9 @@ module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS", [
+      "o9AKK1rh+BobPtmnSM3620gk1+0i5NhdTIZfgCjxhbM=",
+      "ZJr59ZIkRbKUsDGTzScQX8VttB5c4EK7hsUU4WWGInQ=",
+    ]),
   },
 });
