8f3dccdc6ee71fb0f3873e054c0bad7aa6824796
Melanie Rodas <mrodasl@uni.pe>
2022-05-25 12:45:05 -0500

    Update database config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,15 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("MY_HEROKU_URL"),
+  url: env("HEROKU_URL"),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env(
+        "ADMIN_JWT_SECRET",
+        "q1nk3TmLFDuj/T3WwksWb3bDjHL7Pv/dDKDiBJBbORo="
+      ),
+    },
   },
 });
