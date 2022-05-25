afaf3c5eab15aefa4466b5c8526b00de4772a3f0
Melanie Rodas <mrodasl@uni.pe>
2022-05-25 15:29:16 -0500

    updated server.js(added admin)

--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("APP_HOST", "0.0.0.0"),
   port: env.int("NODE_PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "dmelsa5par4$445oladcce#5655%&%&=="),
+    },
+  },
 });
