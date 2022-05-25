2b622402c661bd12ab75981f98e45a049df1e570
Melanie Rodas <mrodasl@uni.pe>
2022-05-25 13:44:39 -0500

    Update production/server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,15 +1,8 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("HEROKU_URL"),
+  url: env("MY_HEROKU_URL"),
+  proxy: true,
   app: {
-    keys: env.array("APP_KEYS"),
-  },
-  admin: {
-    auth: {
-      secret: env(
-        "ADMIN_JWT_SECRET",
-        "q1nk3TmLFDuj/T3WwksWb3bDjHL7Pv/dDKDiBJBbORo="
-      ),
-    },
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
